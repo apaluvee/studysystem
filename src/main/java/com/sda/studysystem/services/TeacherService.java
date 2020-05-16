7df6af0fdfe67c1c370bce568a94f263a0f3653c
Alar Paluvee <alar260@hotmail.com>
2020-05-16 13:20:31 +0300

    Initial commit

---
+++ src/main/java/com/sda/studysystem/services/TeacherService.java
@@ -0,0 +1,47 @@
+package com.sda.studysystem.services;
+
+import com.sda.studysystem.models.Teacher;
+
+import java.util.List;
+
+/**
+ * Service to handle Teacher related operations
+ * @author Alar
+ */
+
+public interface TeacherService {
+
+    /**
+     * To create a new Teacher
+     * @param teacher Teacher
+     */
+    boolean createTeacher(Teacher teacher);
+
+    /**
+     * To update an existing Teacher
+     * @param teacher teacher
+     * @return update result
+     */
+    boolean updateTeacher(Teacher teacher);
+
+    /**
+     * To get a teacher by its id
+     * @param teacherId id of a teacher
+     * @return Teacher
+     */
+    Teacher getById(Long teacherId);
+
+    /**
+     * To get all the Teachers
+     * @return list of all teachers
+     */
+    List<Teacher> getAllTeachers();
+
+    /**
+     * To delete a teacher by its id
+     * @param teacherId teacherId
+     * @return is it deleted
+     */
+    boolean deleteTeacherById(Long teacherId);
+
+}
